c9b475b7441a5518eac71a49fd3a75cdda6fc44e
kizerkul86 <23610008@student.hcmute.edu.vn>
2025-04-19 18:59:27 +0700

    Add files via upload
    add new home page

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Xử lý ảnh số",
+    page_icon="👋",
+)
+st.sidebar.header("Xử Lý Ảnh Số")
+st.write("# Chào mừng bạn đến với Project xử lý ảnh số 👋")
+st.write("## Tôi là Kizer - 23610008 👋")
+
+st.sidebar.success("Hãy chọn menu trên")
+
